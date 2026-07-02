ab03defd80a7b6019d84bdf214e3ae771c17487b
Aswin Ashokan <aswinashokan@Aswins-MacBook-Air.local>
2026-07-03 00:07:59 +0530

    Java Learning base commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+
+    int num = 10;
+    String name = "aswin";
+    boolean isDev = true;
+
+    char gender = 'M';
+    float grade = 76.3F;
+
+
+
+
+}
