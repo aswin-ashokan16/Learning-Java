237932d9bdf97667fa0a1f802879443e8fe2e193
Aswin Ashokan <aswinashokan@Aswins-MacBook-Air.local>
2026-07-06 13:44:05 +0530

    variables problems completed

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,170 @@
 public class Main{
     public static void main(String args[]){
-        System.out.println("Hello World");
+        // Variables
+//        String name = "James Bond";
+//        byte age = 42;
+//        int salary = 80_000;
+//        System.out.println("Name:" + name + " " + "Age:"+ age + " " + "Salary:"+ salary);
+
+       //Celicus to Frahenheit
+//        double celicus = 42.6;
+//        double fahrenheit = celicus * 1.8 + 32;
+//        System.out.println(fahrenheit);
+
+        //Area of rectangle
+//        int w = 30;
+//        int l = 10;
+//        int rectangleArea = w * l;
+//        System.out.println(rectangleArea);
+
+        //area of circle
+//        int r = 22;
+//        double pi = 3.14159265;^R
+//        double circleArea = pi * r * r;
+//        System.out.println(circleArea);
+
+        //simple interest
+//        double p = 7_50_000;
+//        byte n = 5;
+//        double r = 9.7;
+//        double simpleInterest = p * n * r / 100;
+//        System.out.println(simpleInterest);
+
+        // swap using a temp
+//        int a = 10;
+//        int b = 20;
+//        int temp;
+//
+//        System.out.println("Before swap: A" + " " + a);
+//        System.out.println("Before swap: B" + " " + b);
+//
+//       temp = a;
+//       a = b;
+//       b = temp;
+//
+//        System.out.println("After swap: A" + " " + a);
+//        System.out.println("After swap: B" + " " + b);
+
+        //swap without temp
+//        int a = 50;
+//        int b = 70;
+//
+//        System.out.println("Before swap: A" + " " + a);
+//        System.out.println("Before swap: B" + " " + b);
+          //method 1
+//        b = a + b;
+//        a = b - a;
+//        b = b - a;
+         //method 2
+//         a = a ^ b;
+//         b = a ^ b;
+//         a = a ^ b;
+
+//        System.out.println("After swap: A" + " " + a);
+//        System.out.println("After swap: B" + " " + b);
+
+        //Convert Days into years and months
+//        double days = 675;
+//        double years;
+//        double months;
+//        years = days / 365;
+//        months = days / 30;
+//
+//        System.out.println(years);
+//        System.out.println(months);
+
+        //Convert second into Hours and minutes
+//        double seconds = 792;
+//        double hours;
+//        double minutes;
+//        hours = seconds / 3600;
+//        minutes = seconds / 60;
+//        System.out.println("Hours : " + hours);
+//        System.out.println("Minutes : " + minutes);
+
+        //average of 5 numbers
+//        int num1 = 34;
+//        int num2 = 98;
+//        int num3 = 43;
+//        int num4 = 92;
+//        int num5 = 54;
+//        double result = (num1 + num2 + num3 + num4 + num5)/ 5;
+//        System.out.println("Average: " + result);
+
+        //Calculate BMI
+//        double weight = 82;
+//        double height = 168;
+//        double heightInMeter = height / 100;
+//        double BMI = weight / (heightInMeter*heightInMeter);
+//        System.out.println("BMI : " + BMI);
+
+        //Electricity Bill
+//        int unit = 357;
+//        double billAmount = switch ((Integer) unit) {
+//            case Integer u when u <= 100 ->
+//                    0.0;
+//
+//            case Integer u when u <= 400 ->
+//                    (u - 100) * 4.70;
+//
+//            case Integer u when u <= 500 ->
+//                    (300 * 4.70) + ((u - 400) * 6.30);
+//
+//            case Integer u when u <= 600 ->
+//                    (300 * 4.70) + (100 * 6.30) + ((u - 500) * 8.40);
+//
+//            case Integer u when u <= 800 ->
+//                    (300 * 4.70) + (100 * 6.30) + (100 * 8.40) + ((u - 600) * 9.45);
+//
+//            case Integer u when u <= 1000 ->
+//                    (300 * 4.70) + (100 * 6.30) + (100 * 8.40) + (200 * 9.45) + ((u - 800) * 10.50);
+//
+//            case Integer u ->
+//                    (300 * 4.70) + (100 * 6.30) + (100 * 8.40) + (200 * 9.45) + (200 * 10.50) + ((u - 1000) * 11.55);
+//        };
+//        System.out.println(billAmount);
+
+        //percentage
+//        double Tamil = 86.3;
+//        double English = 96.3;
+//        double Maths = 99.3;
+//        double Science = 81.3;
+//        double Social = 89.3;
+//
+//        double Totalpercentage = (Tamil + English + Maths + Science + Social) / 500 * 100;
+//        System.out.println("Total percentage :" + Totalpercentage);
+
+        //Convert rupees into paise
+//        int rupees = 2;
+//        double paise = rupees * 100;
+//        System.out.println("Paise is :" + paise );
+
+        //Profit or loss percentage
+//        double penSellingPrice = 25;
+//        double penCostPrice = 21;
+//        double profit = penSellingPrice - penCostPrice;
+//        double loss = penCostPrice - penSellingPrice;
+//        double profitPercentage = (profit / penCostPrice) * 100;
+//        double lossPercentage = (loss / penCostPrice) * 100;
+//        System.out.println("Profit Percentage :" + profitPercentage);
+//        System.out.println("Loss Percentage :" + lossPercentage);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
-
-    int num = 10;
-    String name = "aswin";
-    boolean isDev = true;
-
-    char gender = 'M';
-    float grade = 76.3F;
-
-
 
 
 }
